44387ddcf886ad8fd2733c6f6840f6af86a0aa8a
pearlhchang <p.chang1802@gmail.com>
2023-04-19 19:11:54 -0700

    finished

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector("button");
-    const img = document.querySelector("img");
+    const img = document.createElement("img");
     function generateMeme() {
         const imagesContainer = document.querySelector("div");
         const endpoint = "https://dog.ceo/api/breeds/image/random";
         fetch(endpoint)
         .then((data) => data.json())
         .then((data) => {
-            // const img = new Image();
-            // console.log(data.message)
             img.src = data.message;
             imagesContainer.appendChild(img);
             imagesContainer.src = img.src;
+            button.style.marginLeft = '133px';
+            button.innerText = 'Next Pup';
+            button.style.fontSize = '40px';
+            button.style.padding = '10px 20px';
         });
     }
     button.addEventListener("click", generateMeme);
 });
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const button = document.querySelector("button");
-//     // const img = document.querySelector("img");
-//     function generateMeme() {
-//         const imagesContainer = document.querySelector("div");
-//         const endpoint = "https://dog.ceo/api/breeds/image/random";
-//         fetch(endpoint)
-//         .then((data) => data.json())
-//         .then((data) => {
-//             const img = new Image();
-//             console.log(data.message)
-//             img.src = data.message;
-//             imagesContainer.appendChild(img);
-//             // imagesContainer.src = img.src;
-//         });
-//     }
-//     button.addEventListener("click", generateMeme);
-// });
