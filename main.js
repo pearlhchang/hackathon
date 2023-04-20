9e6d97727ef57e52b89ae2e4f539d23a4394c42e
pearlhchang <p.chang1802@gmail.com>
2023-04-19 18:30:02 -0700

    finished main.js

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector("button");
+    const img = document.querySelector("img");
     function generateMeme() {
         const imagesContainer = document.querySelector("div");
         const endpoint = "https://dog.ceo/api/breeds/image/random";
         fetch(endpoint)
         .then((data) => data.json())
         .then((data) => {
-            // const memes = data.data.memes;
-            // //iterating through all images on page
-            // const meme = memes[Math.floor(Math.random() * memes.length)];
-            // // select a random meme from the meme array
-            const img = new Image();
-            console.log(data.message)
+            // const img = new Image();
+            // console.log(data.message)
             img.src = data.message;
             imagesContainer.appendChild(img);
-            //setting to a new picture
+            imagesContainer.src = img.src;
         });
-     
     }
-    
     button.addEventListener("click", generateMeme);
-
 });
 
 
+// document.addEventListener("DOMContentLoaded", () => {
+//     const button = document.querySelector("button");
+//     // const img = document.querySelector("img");
+//     function generateMeme() {
+//         const imagesContainer = document.querySelector("div");
+//         const endpoint = "https://dog.ceo/api/breeds/image/random";
+//         fetch(endpoint)
+//         .then((data) => data.json())
+//         .then((data) => {
+//             const img = new Image();
+//             console.log(data.message)
+//             img.src = data.message;
+//             imagesContainer.appendChild(img);
+//             // imagesContainer.src = img.src;
+//         });
+//     }
+//     button.addEventListener("click", generateMeme);
+// });
